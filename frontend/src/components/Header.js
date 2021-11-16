b512442f7f9c17b75999fe1b43a0d65433d41f05
Bernard Sng <bernardsng@gmail.com>
2021-11-16 15:19:00 +0800

    Welcome component

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -3,14 +3,14 @@ import { Navbar, Container } from "react-bootstrap";
 import { ReactComponent as Logo } from '../Images/logo.svg';
 
 const navbarStyle = {
-  backgroundColor: '#eeeee'
+  backgroundColor: '#eeeeee',
 }
 
 const Header = ({title}) => {
   return (
     <Navbar style={navbarStyle} variant="light">
       <Container >
-        <Logo style={ {maxWidth: '12rem', maxHeight:'2rem'} } />
+        <Logo alt={title} style={ {maxWidth: '12rem', maxHeight:'2rem'} } />
       </Container>
     </Navbar>
   );
